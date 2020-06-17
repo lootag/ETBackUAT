df133aad47fae4a1a65ccdaad1a514572c6d2762
Alex <luigi.taglialatela1996@gmail.com>
2020-06-17 18:42:31 +0200

    fixing unit tests

--- UnitTesting/UserRepositoryTest.cs
+++ UnitTesting/UserRepositoryTest.cs
@@ -33,7 +33,7 @@ namespace UnitTesting
         }
 
         [Fact]
-        public void Should_Record_Be_Inserted()
+        public void Should_User_Be_Inserted()
         {
             //Arrange
             string Email = "test.user@test.com";
@@ -61,6 +61,74 @@ namespace UnitTesting
                 Assert.Equal((int)Status.Free, (int)users[0].Status);
             }
             
+        }
+
+        [Fact]
+        public void Should_Status_Be_Updated()
+        {
+            //Arrange
+            const string sqlInsertUser = @"insert into dbo.Users(Email, Hash, Salt, Status) 
+                                            values(@Email, @Hash, @Salt, @Status)";            
+            const string sqlRetrieveId = "select Id from dbo.Users where Email = @Email";
+            const string sqlRetriveStatus = "select Status from dbo.Users where Id = @Id";
+            string Email = "test.email@test.com";
+            byte[] Hash = {1, 2, 3};
+            byte[] Salt = {4, 5, 6};
+            using(IContext context = contextFactory.Create())
+            {
+                //Arrange
+                context.Execute(sqlInsertUser, new
+                {
+                    Email,
+                    Hash,
+                    Salt,
+                    Status = Status.Free
+                });
+                IList<long> idList = context.Query<long>(sqlRetrieveId, new
+                {
+                    Email = Email
+                });
+
+                long Id = idList[0];
+                //Act
+                UserUpdateDto updateDto = new UserUpdateDto(Id, Status.Premium);
+                repository.Update(updateDto, context);
+                context.Commit();
+                IList<Status> newStatusList = context.Query<Status>(sqlRetriveStatus, new
+                {
+                    Id = Id
+                });
+                int newStatus = (int)newStatusList[0];
+                //Assert
+                Assert.Equal((int)Status.Premium, newStatus);
+            }
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Records()
+        {
+            string Email = "test.email@test.com";
+            byte[] Hash = {1, 2, 3};
+            byte[] Salt = {1, 2, 3};
+            using(IContext context = contextFactory.Create())
+            {
+                const string sqlInsertUser = "insert into dbo.Users(Email, Hash, Salt, Status) values(@Email, @Hash, @Salt, @Status)";
+                context.Execute(sqlInsertUser, new
+                {
+                    Email = Email,
+                    Hash = Hash,
+                    Salt = Salt,
+                    Status = Status.Free
+                });
+                context.Commit();
+                IList<UserGetDto> userList = repository.GetByEmail(Email, context);
+                Assert.Equal(1, userList.Count);
+                Assert.Equal(Email, userList[0].Email);
+                Assert.Equal(Hash, userList[0].Hash);
+                Assert.Equal(Salt, userList[0].Salt);
+                Assert.Equal((int)Status.Free, userList[0].Status);
+            }            
+
         }
     }
 }
